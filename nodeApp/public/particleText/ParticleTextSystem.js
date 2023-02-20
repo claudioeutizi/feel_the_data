f83b11d1aa0e8e0173de3156cda8868eb05efcf6
Claudio Eutizi <claudio.eutizi@mail.polimi.it>
2023-02-20 16:16:53 +0100

    Index page modificata e font

--- nodeApp/public/particleText/ParticleTextSystem.js
+++ nodeApp/public/particleText/ParticleTextSystem.js
@@ -1,48 +1,83 @@
 let font;
-let points;
-let bounds;
+let titlePoints;
+let titleBounds;
+let subtitlePoints;
+let subtitleBounds;
 let startTime;
 
+let buttonWidth = 80;
+let buttonHeight = 40;
+let button;
+
 function preload() {
-    font = loadFont('./../Seven Segment.ttf');
+    font = loadFont('./../Gloock.ttf');
 }
 
 function setup() {
     createCanvas(windowWidth, windowHeight);
 
-    points = font.textToPoints(
-        'FEEL THE DATA', 0, 0, 100, {
-        sampleFactor: 1,
+    titlePoints = font.textToPoints(
+        'FEEL THE DATA', 800, -150, 80, {
+        sampleFactor: 0.8,
         simplifyThreshold: 0
     });
 
-    bounds = font.textBounds(
-        'FEEL THE DATA', 0, 0, 200);
+    titleBounds = font.textBounds(
+        'FEEL THE DATA', 0, 0, 250);
+
+    subtitlePoints = font.textToPoints(
+        'The data sonification project.', 300, -300, 50, {
+        sampleFactor: 0.5,
+        simplifyThreshold: 0
+    });
+
+    subtitleBounds = font.textBounds(
+        'The data sonification project.', 0, 0, 100);
 
     cursor(CROSS);
     fill(255, 127);
     noStroke();
     startTime = millis();
+
+    button = createButton('START');
+    button.id('play-button')
+    button.position(windowWidth/2 - 100, windowHeight - windowHeight/4);
+    button.style('font-family','Gloock');
+    button.style('letter-spacing', 2);
+    button.mousePressed();
+}
+
+function mousePressed(){
+    window.location.href = "/map.html";
 }
 
 function draw() {
     noStroke();
-    background(51);
+    background(27, 24, 24);
+
     let elapsedTime = millis() - startTime;
 
     let targetAlpha = elapsedTime >= 600 ? 255 : 0;
     let transparency = lerp(0, targetAlpha, (elapsedTime - 600) / 1000);
     transparency = constrain(transparency, 0, 255);
-    fill(255, transparency);
+    fill(250, transparency);
 
     let jiggle = map(elapsedTime, 2000, 0, 1, 300, true);
-    translate((width - abs(bounds.w)) / 2,
-        (height + abs(bounds.h)) / 2);
+    translate((width - abs(titleBounds.w)) / 2,
+        (height + abs(titleBounds.h)) / 2);
 
-    for (let i = 0; i < points.length; i++) {
-        let p = points[i];
-        fill('rgba(255,255,255,0.5)');
+    for (let i = 0; i < titlePoints.length; i++) {
+        let p = titlePoints[i];
         ellipse(p.x + jiggle * randomGaussian(),
-            p.y + jiggle * randomGaussian(), 5, 5);
+            p.y + jiggle * randomGaussian(), 3, 3);
+    }
+
+    translate((width - abs(subtitleBounds.w)) / 3.5,
+        (height + abs(subtitleBounds.h)) / 4);
+
+    for (let i = 0; i < subtitlePoints.length; i++) {
+        let p = subtitlePoints[i];
+        ellipse(p.x + jiggle * randomGaussian(),
+            p.y + jiggle * randomGaussian(), 2, 2);
     }
 }
